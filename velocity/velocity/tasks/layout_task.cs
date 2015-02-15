164e2a27f0c9f7598cadca0f743cf1657f4ddb26
imakewins <toxic72@gmail.com>
2015-02-15 18:33:03 -0500

    markup added

--- velocity/velocity/tasks/layout_task.cs
+++ velocity/velocity/tasks/layout_task.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
 
 namespace velocity.tasks
 {
     //Handles the calculation of document layout based on the viewport then sends the result to a paint_task
     internal class layout_task
     {
+        private IDocument document_object;
+
+        public layout_task(IDocument document_ref)
+        {
+            document_object = document_ref;
+        }
+
 
     }
 }
