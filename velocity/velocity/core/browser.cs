e486caf142de6773ca0a9486963d3fbe08ee7313
imakewins <toxic72@gmail.com>
2015-02-17 09:31:36 -0500

    started working on the new system.
    removed all my old ideas in favor of the new parallel methods.

---
+++ velocity/velocity/core/browser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace velocity.core
+{
+    //Exposed browser class
+    public class browser
+    {
+        //When a new browser is created
+        public browser()
+        {
+            //Spawn tasks (Layout, Script, Paint, Cache, Input)
+            Parallel.Invoke(spawn_worker_tasks);
+        }
+
+        //Create the tasks using unique pipe id's (Somehow we need to generate unique id's for each pipe as they cannot share the same channel.)
+        //TODO: Create a task that returns a unique pipe id. When we enter multi-process mode we provide the pipe id to the process from the main process.
+        private void spawn_worker_tasks()
+        {
+
+        }
+    }
+}
---
+++ velocity/velocity/core/tasks/layout_task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace velocity.core.tasks
+{
+    //Unexposed class to calculate the layout of a frame based on the viewport (browser control)
+    internal class layout_task
+    {
+    }
+}
